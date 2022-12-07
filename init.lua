92b504d626f0f138b02ef802d77edf6fea4e53e6
tunalad <novica.arsic@aol.com>
2022-12-07 19:11:41 +0100

    Initial commit

---
+++ init.lua
@@ -0,0 +1,67 @@
+-- mod-version:3
+-----------------------------------------------------------------------
+local core = require "core"
+local command = require "core.command"
+local os = require "os"
+--local io = require "io"
+
+local utils = require "lixling/utils"
+
+-----------------------------------------------------------------------
+local plugins_list = {} -- list of the plugins found in the init.lua
+
+-- test function
+local function hello_world()-- {{{
+    core.log("hello world ayy")
+    local thing = os.execute("pwd")
+end-- }}}
+	
+-- function gets the table from main config file and assigns it to the `plugins_list` in this file
+local function get_plugins_list(plug_list)-- {{{
+    plugins_list = plug_list
+end-- }}}
+
+-- logs out plugins list 
+local function print_plugins_list()-- {{{
+    for plug in pairs(plugins_list) do
+        core.log(plug .. ": " .. plugins_list[plug])
+    end
+end-- }}}
+
+-- looks for unlisted files in the plugins dir
+local function clear_plugins()-- {{{
+    local dir_plugs = utils.dir_lookup("plugins/")
+
+    for f in ipairs(dir_plugs) do
+        if(not plugins_list[dir_plugs[f]:sub(1, -5)]) then
+            core.log("LIXLING: file '" .. dir_plugs[f] .. "' can be deleted")
+        end
+    end
+
+    command.perform("core:open-log")
+end-- }}}
+
+-- Downloads the plugin (if it's not already installed)
+local function download_plugins()-- {{{
+    local dir_plugs = utils.dir_lookup("plugins/")
+
+    for plug in pairs(plugins_list) do
+        if not utils.array_has_value(dir_plugs, plug..".lua") then
+            if utils.string_ends_with(plugins_list[plug], ".lua") then
+                utils.curl(plug..".lua", plugins_list[plug])
+                core.log("LIXLING: Downloaded '".. plug .. ".lua'")
+            end
+        else
+                core.log("LIXLING: '".. plug .. " plugin' is already installed.")
+        end
+    end
+end-- }}}
+
+-----------------------------------------------------------------------
+
+command.add("core.docview", {["lixling:install"] = download_plugins})
+command.add("core.docview", {["lixling:clear"] = clear_plugins})
+--command.add("core.docview", {["lixling:update"] = hello_world})
+--command.add("core.docview", {["lixling:upgrade"] = hello_world})
+
+return { get_plugins_list = get_plugins_list }
---
+++ utils.lua
@@ -0,0 +1,35 @@
+local M = {}
+
+-- Lists items in a directory and returns an array
+function M.dir_lookup(dir)-- {{{
+	local files_array = {}
+
+	for file in io.popen("ls -p ".. dir .." "):lines() do
+		table.insert(files_array, file)
+	end
+    
+    return files_array
+end-- }}}
+
+-- Checks if array has value in it
+function M.array_has_value(table, value)-- {{{
+    for item in pairs(table) do
+        if table[item] == value then
+            return true
+        end
+    end
+
+    return false
+end-- }}}
+
+-- Checks if string ends with
+function M.string_ends_with(str, ending)-- {{{
+	return ending == "" or str:sub(-#ending) == ending
+end-- }}}
+
+-- CURL Downloading
+function M.curl(file, link)
+    -- should be async
+    io.popen("curl -o 'plugins/".. file .. "' -s ".. link.. ""):read("*a")
+end
+return M
