4ef4003673aa5b71006b0c6f4f5e6891087965c7
tunalad <novica.arsic@aol.com>
2023-10-02 20:27:26 +0200

    minor code cleaning

--- init.lua
+++ init.lua
@@ -1,22 +1,22 @@
 -- mod-version:3
 -----------------------------------------------------------------------
-local core = require "core"
-local command = require "core.command"
-local os = require "os"
+local core = require("core")
+local command = require("core.command")
+local os = require("os")
 
-local utils = require "lixling/utils"
+local utils = require("lixling/utils")
 
 -----------------------------------------------------------------------
 local plugins_list = {} -- list of the plugins found in the init.lua
 local plugins_path = nil
 
 -- Function gets the table from main config file and assigns it to the `plugins_list` in this file
-local function plugins_old(plug_path, plug_list)-- {{{
-    plugins_path = plug_path or USERDIR.."/plugins/"
+local function plugins_old(plug_path, plug_list) -- {{{
+    plugins_path = plug_path or USERDIR .. "/plugins/"
     plugins_list = plug_list
-end-- }}}
+end -- }}}
 
-local function plugins(...)-- {{{
+local function plugins(...) -- {{{
     local param_num = select("#", ...)
 
     -- single parameter
@@ -26,7 +26,7 @@ local function plugins(...)-- {{{
             core.log("LIXLING ERROR: Invalid parameter: expected table.")
             return
         end
-        plugins_path = USERDIR.."/plugins/"
+        plugins_path = USERDIR .. "/plugins/"
         plugins_list = param1
 
     -- two parameters
@@ -38,18 +38,17 @@ local function plugins(...)-- {{{
         end
         plugins_path = param1
         plugins_list = param2
-    else 
+    else
         core.log("LIXLING ERROR: Invalid number of parameters: expected 1 or 2.")
     end
-
-end-- }}}
+end -- }}}
 
 -- Looks for unlisted files in the plugins dir
-local function clear_plugins()-- {{{
+local function clear_plugins() -- {{{
     core.log("LIXLING CLEAR: Running the clearing process. Please wait.")
 
     local dir_plugs = utils.dir_lookup(plugins_path)
-
+    core.log(plugins_path)
     local clear_list = {}
     local clear_size = 0
 
@@ -57,131 +56,140 @@ local function clear_plugins()-- {{{
         -- If plugin ends with `.lua` AND is in list
         if utils.string_ends_with(dir_plugs[f], ".lua") and plugins_list[dir_plugs[f]:sub(1, -5)] == nil then
             table.insert(clear_list, dir_plugs[f])
-            clear_size=clear_size+1
+            clear_size = clear_size + 1
 
-            core.log("LIXLING CLEAR: File '"..dir_plugs[f].."' added to exile list.")
+            core.log("LIXLING CLEAR: File '" .. dir_plugs[f] .. "' added to exile list.")
 
         -- If plugin ends with `/` AND is in list
         elseif utils.string_ends_with(dir_plugs[f], "/") and plugins_list[dir_plugs[f]:sub(1, -2)] == nil then
             table.insert(clear_list, dir_plugs[f])
-            clear_size=clear_size+1
+            clear_size = clear_size + 1
 
-            core.log("LIXLING CLEAR: Folder '"..dir_plugs[f].."' added to exile list.")
+            core.log("LIXLING CLEAR: Folder '" .. dir_plugs[f] .. "' added to exile list.")
         end
     end
 
     -- Handles clearing user input
     if clear_size > 0 then
-        core.command_view:enter("LIXLING CLEAR: Found ".. clear_size .." unlisted plugins, exile them? (y/N)", {
+        core.command_view:enter("LIXLING CLEAR: Found " .. clear_size .. " unlisted plugins, exile them? (y/N)", {
             submit = function(input)
-                if(string.lower(input) == "y" or string.lower(input) == "yes" ) then
+                if string.lower(input) == "y" or string.lower(input) == "yes" then
                     for plug in ipairs(clear_list) do
-                        core.log("LIXLING CLEAR: Moving: '".. clear_list[plug].."'.")
-                        io.popen("mkdir "..USERDIR.."/lixling/exiled"):read()
-                        os.rename(plugins_path..clear_list[plug], USERDIR.."/lixling/exiled/"..clear_list[plug])
+                        core.log("LIXLING CLEAR: Moving: '" .. clear_list[plug] .. "'.")
+                        io.popen("mkdir " .. USERDIR .. "/lixling/exiled"):read()
+                        os.rename(plugins_path .. clear_list[plug], USERDIR .. "/lixling/exiled/" .. clear_list[plug])
                     end
-                    core.log("LIXLING CLEAR: ".. clear_size .. " plugins exiled. You can find them in '"..USERDIR.."/lixling/exiled'.") 
+                    core.log(
+                        "LIXLING CLEAR: "
+                            .. clear_size
+                            .. " plugins exiled. You can find them in '"
+                            .. USERDIR
+                            .. "/lixling/exiled'."
+                    )
                 end
-            end
+            end,
         })
         return 0
     end
     core.log("LIXLING CLEAR: No unlisted plugins found.")
-end-- }}}
+end -- }}}
 
 -----------------------------------------------------------------------
 -- DOWNLOADING
 -----------------------------------------------------------------------
+
 -- RAW .LUA FILE
-local function download_raw(dir_plugs, plugins_list, plug)-- {{{
-    if not utils.array_has_value(dir_plugs, plug..".lua") then
+local function download_raw(dir_plugs, plugins_list, plug) -- {{{
+    if not utils.array_has_value(dir_plugs, plug .. ".lua") then
         if utils.string_ends_with(plugins_list[plug][1], ".lua") then
-            utils.curl(plug..".lua", plugins_list[plug][1])
-            core.log("LIXLING INSTALL: Downloaded '".. plug .. ".lua'")
+            utils.curl(plug .. ".lua", plugins_list[plug][1])
+            core.log("LIXLING INSTALL [raw]: Downloaded '" .. plug .. ".lua'")
         end
     end
-end-- }}}
+end -- }}}
 
 -- GIT REPO CLONE
-local function download_repo(plugins_list, plug, branch)-- {{{
+local function download_repo(plugins_list, plug, branch) -- {{{
     branch = branch or "master"
     if utils.string_ends_with(plugins_list[plug][1], ".git") then
-        local status = utils.git_clone(plugins_path..plug, plugins_list[plug][1], branch)
+        local status = utils.git_clone(plugins_path .. plug, plugins_list[plug][1], branch)
 
         if status then
-            core.log("LIXLING INSTALL: Downloaded '" .. plug .. "'.")
+            core.log("LIXLING INSTALL [repo]: Downloaded '" .. plug .. "'.")
             if plugins_list[plug][3] ~= nil then
-                os.execute("cd plugins/".. plug .."; "..plugins_list[plug][3])
+                os.execute("cd plugins/" .. plug .. "; " .. plugins_list[plug][3])
             end
         end
     end
-end-- }}}
+end -- }}}
 
-local function download_plugins()-- {{{
+local function download_plugins() -- {{{
     core.log("LIXLING INSTALL: Running the install process. Please wait.")
 
     local dir_plugs = utils.dir_lookup(plugins_path)
-    io.popen("mkdir "..USERDIR.."/plugins/"):read()
+    io.popen("mkdir " .. USERDIR .. "/plugins/"):read()
 
     core.add_thread(function()
         for plug in pairs(plugins_list) do
             -- single file + default git branch
-            if (#plugins_list[plug] == 1) then
+            if #plugins_list[plug] == 1 then
                 download_raw(dir_plugs, plugins_list, plug)
                 download_repo(plugins_list, plug)
 
             -- git branch handling
-            elseif (#plugins_list[plug] == 2) then
+            elseif #plugins_list[plug] == 2 then
                 download_repo(plugins_list, plug, plugins_list[plug][2])
 
             -- git branch + hook handle
             elseif (#plugins_list[plug] == 3) and (#plugins_list[plug][2] ~= 0) then
                 download_repo(plugins_list, plug, plugins_list[plug][2])
-            
+
             -- if in old srting format
             elseif type(plugins_list[plug]) == "string" then
-                local dummy = { [plug] = {plugins_list[plug]} }
+                local dummy = { [plug] = { plugins_list[plug] } }
 
-                download_raw(dir_plugs, dummy , plug)
+                download_raw(dir_plugs, dummy, plug)
                 download_repo(dummy, plug)
             end
         end
         core.log("LIXLING INSTALL: All plugins have been downloaded.")
     end)
-end-- }}}
-
+end -- }}}
 
 -----------------------------------------------------------------------
 -- UPDATING
 -----------------------------------------------------------------------
--- RAW .LUA FILE
-local function update_raw(dir_plugs, plugins_list, plug)-- {{{
-    -- if IS_LISTED and IS_RAW_LUA_FILE_LINK and IS_DOWNLOADED
-    if utils.diff(plugins_path..plug..".lua", "<(curl -s ".. plugins_list[plug][1]..")")
-            and utils.string_ends_with(plugins_list[plug][1], ".lua")and utils.array_has_value(dir_plugs, plug..".lua") then
 
-        utils.curl(plug..".lua", plugins_list[plug][1])
-        core.log("LIXLING UPDATE: '".. (plug..".lua") .."'.")
+-- RAW .LUA FILE
+local function update_raw(dir_plugs, plugins_list, plug) -- {{{
+    -- if IS_LISTED and IS_RAW_LUA_FILE_LINK and IS_DOWNLOADED
+    if
+        utils.diff(plugins_path .. plug .. ".lua", "<(curl -s " .. plugins_list[plug][1] .. ")")
+        and utils.string_ends_with(plugins_list[plug][1], ".lua")
+        and utils.array_has_value(dir_plugs, plug .. ".lua")
+    then
+        utils.curl(plug .. ".lua", plugins_list[plug][1])
+        core.log("LIXLING UPDATE [raw]: '" .. (plug .. ".lua") .. "'.")
     end
-end-- }}}
+end -- }}}
 
 -- GIT REPO PULL
-local function update_repo(plugins_list, plug, branch)-- {{{
+local function update_repo(plugins_list, plug, branch) -- {{{
     branch = branch or "master"
     if utils.string_ends_with(plugins_list[plug][1], ".git") then
-        local status = utils.git_pull(plugins_path..plug, branch)
+        local status = utils.git_pull(plugins_path .. plug, branch)
 
         if not status == "Already up to date." then
-            core.log("LIXLING UPDATE: '".. plug .. "' repo updated.")
+            core.log("LIXLING UPDATE [repo]: '" .. plug .. "' repo updated.")
 
             if plugins_list[plug][3] ~= nil then
-                os.execute("cd plugins/".. plug .."; "..plugins_list[plug][3])
+                os.execute("cd plugins/" .. plug .. "; " .. plugins_list[plug][3])
             end
         end
     end
-end-- }}}
+end -- }}}
 
-local function update_plugins()-- {{{
+local function update_plugins() -- {{{
     core.log("LIXLING UPDATE: Running the update process. Please wait.")
 
     local dir_plugs = utils.dir_lookup(plugins_path)
@@ -189,12 +197,12 @@ local function update_plugins()-- {{{
     core.add_thread(function()
         for plug in pairs(plugins_list) do
             -- single file + default git branch
-            if (#plugins_list[plug] == 1) then
+            if #plugins_list[plug] == 1 then
                 update_raw(dir_plugs, plugins_list, plug)
                 update_repo(plugins_list, plug)
 
             -- git branch handling
-            elseif (#plugins_list[plug] == 2) then
+            elseif #plugins_list[plug] == 2 then
                 update_repo(plugins_list, plug, plugins_list[plug] == 2)
 
             -- git branch + hook handle
@@ -203,23 +211,23 @@ local function update_plugins()-- {{{
 
             -- if in old srting format
             elseif type(plugins_list[plug]) == "string" then
-                local dummy = { [plug] = {plugins_list[plug]} }
+                local dummy = { [plug] = { plugins_list[plug] } }
 
-                update_raw(dir_plugs, dummy , plug)
+                update_raw(dir_plugs, dummy, plug)
                 update_repo(dummy, plug)
             end
         end
         core.log("LIXLING UPDATE: All plugins are up to date.")
     end)
-
-end-- }}}
+end -- }}}
 
 -- Upgrades self
-local function upgrade_self()-- {{{
+local function upgrade_self() -- {{{
     core.log("LIXLING UPGRADE: Running the upgrade process. Please wait.")
 
     core.add_thread(function()
-        local status = utils.git_pull(USERDIR.."/lixling/")
+        local status = utils.git_pull(USERDIR .. "/lixling/")
+        core.log(status)
 
         if not status == "Already up to date." then
             core.log("LIXLING UPGRADE: Upgraded to the latest version.")
@@ -227,7 +235,7 @@ local function upgrade_self()-- {{{
         end
         core.log("LIXLING UPGRADE: Already up to date.")
     end)
-end-- }}}
+end -- }}}
 
 -----------------------------------------------------------------------
 
--- utils.lua
+++ utils.lua
@@ -1,20 +1,20 @@
-local core = require "core"
+local core = require("core")
 
 local M = {}
 
 -- Lists items in a directory and returns an array
-function M.dir_lookup(dir)-- {{{
-	local files_array = {}
+function M.dir_lookup(dir) -- {{{
+    local files_array = {}
 
-	for file in io.popen("ls -p ".. dir .." "):lines() do
-		table.insert(files_array, file)
-	end
-    
+    for file in io.popen("ls -p " .. dir .. " "):lines() do
+        table.insert(files_array, file)
+    end
+
     return files_array
-end-- }}}
+end -- }}}
 
 -- Checks if array has value in it
-function M.array_has_value(table, value)-- {{{
+function M.array_has_value(table, value) -- {{{
     for item in pairs(table) do
         if table[item] == value then
             return true
@@ -22,27 +22,32 @@ function M.array_has_value(table, value)-- {{{
     end
 
     return false
-end-- }}}
+end -- }}}
 
 -- Checks if string ends with
-function M.string_ends_with(str, ending)-- {{{
-	return ending == "" or str:sub(-#ending) == ending
-end-- }}}
+function M.string_ends_with(str, ending) -- {{{
+    return ending == "" or str:sub(-#ending) == ending
+end -- }}}
 
 -- CURL Downloading
-function M.curl(file, link)-- {{{
-    local curl = process.start { "sh", "-c", "curl -o 'plugins/".. file .. "' -s ".. link.. " && echo 'file downloaded'" }
+function M.curl(file, link) -- {{{
+    local curl =
+        process.start({ "sh", "-c", "curl -o 'plugins/" .. file .. "' -s " .. link .. " && echo 'file downloaded'" })
 
-    while curl:running() do coroutine.yield(0.1) end
-    if curl:read_stdout() == "file downloaded" then return true end
+    while curl:running() do
+        coroutine.yield(0.1)
+    end
+    if curl:read_stdout() == "file downloaded" then
+        return true
+    end
 
     return false
-end-- }}}
+end -- }}}
 
 -- UNIX diff
-function M.diff(old_file, new_file)-- {{{
+function M.diff(old_file, new_file) -- {{{
     core.add_thread(function()
-        local diff = process.start { "sh", "-c", "diff ".. old_file .." ".. new_file .." | wc -l" }
+        local diff = process.start({ "sh", "-c", "diff " .. old_file .. " " .. new_file .. " | wc -l" })
 
         while diff:running() do
             coroutine.yield(0.1)
@@ -54,31 +59,37 @@ function M.diff(old_file, new_file)-- {{{
 
         return false
     end)
-end-- }}}
+end -- }}}
 
 -- GIT repo updating
-function M.git_pull(local_path, branch)-- {{{
+function M.git_pull(local_path, branch) -- {{{
     branch = branch or "master"
 
-    local result = process.start { "sh", "-c", "git --git-dir ".. local_path .."/.git pull origin ".. branch .."" }
-    while result:running() do coroutine.yield(0.1) end
-
-    --local result = io.popen("git --git-dir ".. local_path .."/.git pull origin ".. branch ..""):read("*a")
+    local result = process.start({ "sh", "-c", "git --git-dir " .. local_path .. "/.git pull origin " .. branch .. "" })
+    while result:running() do
+        coroutine.yield(0.1)
+    end
 
     return result:read_stdout()
-end-- }}}
+end -- }}}
 
 -- GIT repo clone
-function M.git_clone(local_path, link, branch)-- {{{
+function M.git_clone(local_path, link, branch) -- {{{
     branch = branch or "master"
 
-    local result = process.start { "sh", "-c", "git clone -b ".. branch .." '".. link .."' ".. local_path .."/ && echo -n 'repo cloned'"}
-    while result:running() do coroutine.yield(0.1) end
+    local result = process.start({
+        "sh",
+        "-c",
+        "git clone -b " .. branch .. " '" .. link .. "' " .. local_path .. "/ && echo -n 'repo cloned'",
+    })
+    while result:running() do
+        coroutine.yield(0.1)
+    end
 
-    --local result = io.popen("git clone -b ".. branch .." '".. link .."' ".. local_path .."/ && echo -n 'repo cloned'"):read("*a")
-
-    if result:read_stdout() == "repo cloned" then return true end
+    if result:read_stdout() == "repo cloned" then
+        return true
+    end
     return false
-end-- }}}
+end -- }}}
 
 return M
